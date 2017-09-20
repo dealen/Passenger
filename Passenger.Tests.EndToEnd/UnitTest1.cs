305bf3e4d577d681b20f878a49425bc2251b7741
dealen <marcickiewiczjakub@gmail.com>
2017-09-20 15:48:31 +0200

    Initial commit

---
+++ Passenger.Tests.EndToEnd/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Passenger.Tests.EndToEnd
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
