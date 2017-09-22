0989d46138abeb1cfce39ef9d70efe1ea3603627
dealen <marcickiewiczjakub@gmail.com>
2017-09-22 12:42:33 +0200

    test commit

--- Passenger.Api/Startup.cs
+++ Passenger.Api/Startup.cs
@@ -26,6 +26,7 @@ namespace Passenger.Api
             services.AddMvc();
         }
 
+        // dsds
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
